4fcfb827b8d65875c3b83180eed912b0c4cde2d4
Nitin Sijwali <NITIN.SIJWALI@questdot.io>
2021-01-20 14:41:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import {
+	MapContainer,
+	TileLayer,
+	Marker,
+	Popup,
+	useMapEvents,
+} from 'react-leaflet';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [pos, setPos] = useState([51.505, -0.09]);
+	const [text, setText] = useState('');
+	const [zoom, setZoom] = useState(13);
+	function LocationMarker() {
+		const map = useMapEvents({
+			click() {
+				map.locate();
+			},
+			locationfound(e) {
+				setPos(e.latlng);
+				setText('you are here');
+				map.flyTo(e.latlng, map.getZoom());
+			},
+		});
+		return (
+			<Marker position={pos}>{text !== '' && <Popup>{text}</Popup>}</Marker>
+		);
+	}
+	return (
+		<>
+			<h1>🌝🌚</h1>
+			<input type='checkbox' id='switch' />
+			<label for='switch'></label>
+			<section class='dark-mode'></section>
+			<div className='app'>
+				<MapContainer center={pos} zoom={zoom} scrollWheelZoom={true}>
+					<TileLayer
+						url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+						// attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+					/>
+					<LocationMarker />
+				</MapContainer>
+			</div>
+		</>
+	);
 }
 
 export default App;
